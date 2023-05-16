d9fe1703451930c64f22bcf3da0680ecbfd60a8b
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-05-16 23:24:00 +0300

    bug fix

--- src/components/header/header/Head.tsx
+++ src/components/header/header/Head.tsx
@@ -10,7 +10,7 @@ const Head = (): ReactElement => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <NavLink to="/" className={`${styles.logo}`}>
+        <NavLink to="/" className={searchValue !== "" ? `${styles.logo} ${styles.nonactive}` : `${styles.logo}`}>
           <h2>LOGO</h2>
         </NavLink>
         <div className={`${styles.container_catalog}`}>
